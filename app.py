b7c14fe1093d5371a9956500c0db72bc86b17a3e
mikey <mikey@hey.com>
2022-11-17 06:55:41 -0600

    total_distance callback function

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ df = pd.read_csv('data/numeric_data.csv', parse_dates=['Date'],
 # create the fig
 fig = px.line(data_frame=df, y='Distance')
 
+total_ran = int(df['Distance'].sum())
 
 # create layout
 app.layout = html.Div(children=[
     
-    html.H1(children='hello, world'),
+    html.H1(children='Run 100 Miles'),
     
-    html.Div(children='i like to run'),
+    html.Div(id='total_distance', 
+             children='{} Miles'.format(total_ran)),
     
     dcc.DatePickerRange(id='date_filter',
                         start_date=df.index.min(),
@@ -44,6 +46,16 @@ def updateGraph(start_date, end_date):
         raise dash.exceptions.PreventUpdate
     else:
         return px.line(data_frame=df[start_date:end_date], y='Distance')
+
+@app.callback(
+    Output('total_distance', 'children'),
+    Input('date_filter', 'start_date'),
+    Input('date_filter', 'end_date'))
+def updateTotalDistance(start_date, end_date):
+    if not start_date or not end_date:
+        raise dash.exceptions.PreventUpdate
+    else:
+        return '{} Miles'.format(int(df[start_date:end_date]['Distance'].sum()))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
